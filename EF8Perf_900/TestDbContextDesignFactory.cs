a00041373f4fb70a2a8d9b9a000c3ae5b05f0496
Evgeny Muryshkin <evgeny.muryshkin@avocado.com.au>
2024-03-26 10:45:06 +1100

    add EF9 preview to test

---
+++ EF8Perf_900/TestDbContextDesignFactory.cs
@@ -0,0 +1,21 @@
+﻿using crosscutting.sql;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Debug;
+using System;
+using System.Diagnostics;
+
+namespace test.sql
+{
+    public class TestDbContextDesignFactory : AbstractContextDesignFactory<tstTestDBContext>
+    {
+        public static string ConnectionString = "Data Source=.\\;Initial catalog=test;Integrated Security=False;User ID=scms;Password=scms;";
+        public override tstTestDBContext CreateDbContext(Guid platformId, params string[] args)
+            => InternalCreateDbContext(
+                platformId,
+                ConnectionString,
+                tstTestDBContext.SchemaName,
+                args);
+    }
+}
